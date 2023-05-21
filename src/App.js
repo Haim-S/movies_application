a880923b07dc613bb5eb5cfdde2b9aa5a732ae8d
HaimSlbah <26life72@gmail.com>
2023-05-21 10:51:51 +0300

    fix the login page and the firebase config

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import {Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
 
 
 
 function App() {
   return (
     <>
+    <AuthContextProvider> 
     <Navbar/>
     <Routes>
       <Route path="/" element={<HomePage/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/account" element={<Account/>}/>
     </Routes>
+    </AuthContextProvider>
     </>
   );
 }
