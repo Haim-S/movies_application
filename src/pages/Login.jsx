4263937929ee9e3f184218a29ee8532868f8346a
HaimSlbah <26life72@gmail.com>
2023-05-21 19:19:29 +0300

    I finish this appliction

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React, {useRef, useState} from 'react'
+import {Link , useNavigate} from "react-router-dom"
+import { UserAuth } from '../context/AuthContext';
 
 const Login = () => {
+
+  const [error, setError] = useState('');
+  const RefEmail = useRef();
+  const RefPassword = useRef();
+  const {user, LogIn} = UserAuth();
+  const navigate = useNavigate();
+  
+  
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+    try {
+      await LogIn(RefEmail.current.value, RefPassword.current.value);
+      navigate('/')
+    } catch (error) {
+      console.log(error);
+      setError(error.message)
+    }
+  }
+
   return (
     <div className='w-full h-screen'>
     <img
@@ -14,9 +36,10 @@ const Login = () => {
         <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
           <div className='max-w-[320px] mx-auto py-16'>
             <h1 className='text-3xl font-bold'>Sign In</h1>
-            <form className='w-full flex flex-col py-4'>
-              <input className='p-3 my-2 bg-gray-700 rounded' type="email" placeholder='Email' autoComplete='email'/>
-              <input className='p-3 my-2 bg-gray-700 rounded' type="password" placeholder='Password' autoComplete='current-password'/>
+            {error ? <p className='p-3 bg-red-400 my-2'>{error}</p> : null}
+            <form onSubmit={handleSubmit} className='w-full flex flex-col py-4'>
+              <input ref={RefEmail} className='p-3 my-2 bg-gray-700 rounded' type="email" placeholder='Email' autoComplete='email'/>
+              <input ref={RefPassword} className='p-3 my-2 bg-gray-700 rounded' type="password" placeholder='Password' autoComplete='current-password'/>
               <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign In</button>
               <div className='flex justify-between items-center text-sm text-gray-600'>
                 <p><input className='mr-2' type="checkbox"/>Remember me</p>
